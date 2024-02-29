4040864156db0ba95ffb03edfe2dad657ac86a19
Tobias Theliander <tobthe0916@staden.gotheborg.net>
2024-02-29 12:01:17 +0100

    lol

--- Assets/scripts/parrotspawnerscriptt.cs
+++ Assets/scripts/parrotspawnerscriptt.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class parrotspawnerscriptt : MonoBehaviour
@@ -8,6 +9,9 @@ public class parrotspawnerscriptt : MonoBehaviour
 
     public GameObject parrot;
 
+    bool parrotdragging = false;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +21,7 @@ public class parrotspawnerscriptt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        /*
         if(Input.GetMouseButtonDown(0))
         {
             Vector3 newparrotpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -25,6 +29,31 @@ public class parrotspawnerscriptt : MonoBehaviour
             GameObject newparrot = Instantiate(parrot, newparrotpos,Quaternion.identity);
             Rigidbody2D rb = newparrot.GetComponent<Rigidbody2D>();
             rb.velocity = rb.velocity - new Vector2(-20, 0);
+        }*/
+
+
+        if (Input.GetMouseButtonUp(0) && parrotdragging)    
+        {
+
+            
+            GameObject newparrot = Instantiate(parrot, transform.position, Quaternion.identity);
+            Rigidbody2D rb = newparrot.GetComponent<Rigidbody2D>();
+            rb.velocity = rb.velocity - new Vector2(-20, 0);
+
+
+            parrotdragging = false;
+            Debug.Log("sjukt");
         }
+
+
+        
+
     }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("dra parroten");
+        parrotdragging = true;
+    }
+  
 }
