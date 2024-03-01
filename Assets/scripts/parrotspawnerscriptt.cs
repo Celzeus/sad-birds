9ece6c54e1bda849f61040b57df5794c7dd0aa59
Tobias Theliander <tobthe0916@staden.gotheborg.net>
2024-03-01 08:59:57 +0100

    Physics + game changes

--- Assets/scripts/parrotspawnerscriptt.cs
+++ Assets/scripts/parrotspawnerscriptt.cs
@@ -2,47 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class parrotspawnerscriptt : MonoBehaviour
 {
 
 
     public GameObject parrot;
+    public GameObject parrotslefttext;
 
     bool parrotdragging = false;
 
-    
+    public float forcemulti = 10;
+    public float forcemaxi = 20;
+
+    public int noparrots = 3;
+
+
+    TextMeshProUGUI TMpro;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TMpro = parrotslefttext.GetComponent<TextMeshProUGUI>();
+        TMpro.text = " parrots left " + noparrots;
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        if(Input.GetMouseButtonDown(0))
+        
+        
+       
+
+
+        if (Input.GetMouseButtonUp(0) && parrotdragging && noparrots > 0)    
         {
-            Vector3 newparrotpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            newparrotpos.z = 0;
-            GameObject newparrot = Instantiate(parrot, newparrotpos,Quaternion.identity);
-            Rigidbody2D rb = newparrot.GetComponent<Rigidbody2D>();
-            rb.velocity = rb.velocity - new Vector2(-20, 0);
-        }*/
+            parrotdragging = false;
 
+            noparrots--;
+            TMpro.text = " parrots left " + noparrots;
 
-        if (Input.GetMouseButtonUp(0) && parrotdragging)    
-        {
-
-            
             GameObject newparrot = Instantiate(parrot, transform.position, Quaternion.identity);
             Rigidbody2D rb = newparrot.GetComponent<Rigidbody2D>();
-            rb.velocity = rb.velocity - new Vector2(-20, 0);
+            
+
+            Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousepos.z = 0;
+
+            rb.velocity = (transform.position - mousepos) * forcemulti;
+            if (rb.velocity.magnitude > forcemaxi)
+            {
+                rb.velocity = rb.velocity.normalized * forcemaxi;
+            }
 
 
-            parrotdragging = false;
-            Debug.Log("sjukt");
+            
+            
         }
 
 
@@ -52,8 +67,14 @@ public class parrotspawnerscriptt : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Debug.Log("dra parroten");
-        parrotdragging = true;
+        if (noparrots > 0)
+        {
+            
+            parrotdragging = true;
+        }
+       
+
+
     }
   
 }
